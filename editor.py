e49cd2fb5c78ceefcb5c4b500be3c3420090cbb9
vaclavtruhlarik <truhlarik.v@seznam.cz>
2023-11-11 20:47:48 +0100

    Drawing support lines in editor

--- editor.py
+++ editor.py
@@ -14,6 +14,11 @@ class Editor:
 		self.origin = vector()
 		self.pan_active = False
 		self.pan_offset = vector()
+
+		# support lines
+		self.support_line_surf = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+		self.support_line_surf.set_colorkey("green")
+		self.support_line_surf.set_alpha(30)
 		
 	# input
 	def event_loop(self):
@@ -42,7 +47,31 @@ class Editor:
 		if self.pan_active:
 			self.origin = vector(mouse_pos()) - self.pan_offset
 
+	# drawing
+	def draw_tile_lines(self):
+		cols = WINDOW_WIDTH // TILE_SIZE
+		rows = WINDOW_HEIGHT // TILE_SIZE
+
+		origin_offset = vector(
+			x = self.origin.x - int(self.origin.x / TILE_SIZE) * TILE_SIZE,
+			y = self.origin.y - int(self.origin.y / TILE_SIZE) * TILE_SIZE)
+		
+		self.support_line_surf.fill('green')
+		
+		for col in range(cols + 1):
+			x = origin_offset.x + col * TILE_SIZE
+			pygame.draw.line(self.support_line_surf, LINE_COLOR, (x, 0), (x, WINDOW_HEIGHT))
+
+		for row in range(rows):
+			y = origin_offset.y + row *TILE_SIZE
+			pygame.draw.line(self.support_line_surf, LINE_COLOR, (0, y), (WINDOW_WIDTH, y))
+
+		self.display_surface.blit(self.support_line_surf, (0,0))
+	
 	def run(self, dt):
 		self.display_surface.fill('white')
 		self.event_loop()
+
+		# drawing
+		self.draw_tile_lines()
 		pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
