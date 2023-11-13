7333ddd0c8380beb6a931013a9a914b1c55d909d
vaclavtruhlarik <truhlarik.v@seznam.cz>
2023-11-13 22:14:03 +0100

    Creating timer, removing objects except for player and sky

--- code/editor.py
+++ code/editor.py
@@ -5,6 +5,7 @@ from pygame.mouse import get_pos as mouse_pos
 from pygame.image import load
 from settings import *
 from support import *
+from timer import Timer
 
 from menu import Menu
 
@@ -39,6 +40,7 @@ class Editor:
 		# objects
 		self.canvas_objects = pygame.sprite.Group()
 		self.object_drag_active = False
+		self.object_timer = Timer(400)
 
 		# player
 		CanvasObject(
@@ -115,6 +117,11 @@ class Editor:
 			if value['frame index'] >= value['length']:
 				value['frame index'] = 0
 	
+	def mouse_on_object(self):
+		for sprite in self.canvas_objects:
+			if sprite.rect.collidepoint(mouse_pos()):
+				return sprite
+	
 	# input
 	def event_loop(self):
 		for event in pygame.event.get():
@@ -179,15 +186,24 @@ class Editor:
 					self.check_neighbors(current_cell)
 					self.last_selected_cell = current_cell
 			else:
-				CanvasObject(
-					pos = mouse_pos(),
-					frames = self.animations[self.selection_index]['frames'],
-					tile_id = self.selection_index,
-					origin = self.origin,
-					group = self.canvas_objects)
+				if not self.object_timer.active:
+					CanvasObject(
+						pos = mouse_pos(),
+						frames = self.animations[self.selection_index]['frames'],
+						tile_id = self.selection_index,
+						origin = self.origin,
+						group = self.canvas_objects)
+					self.object_timer.activate()
 	
 	def canvas_remove(self):
 		if mouse_buttons()[2] and not self.menu.rect.collidepoint(mouse_pos()):
+			# delete object
+			selected_object = self.mouse_on_object()
+			if selected_object:
+				if EDITOR_DATA[selected_object.tile_id]['style'] not in ('player', 'sky'):
+					selected_object.kill()
+
+			# delete tiles
 			if self.canvas_data:
 				current_cell = self.get_current_cell()
 				if current_cell in self.canvas_data:
@@ -273,6 +289,7 @@ class Editor:
 		# updating
 		self.animation_update(dt)
 		self.canvas_objects.update(dt)
+		self.object_timer.update()
 
 		# drawing
 		self.display_surface.fill('gray')
